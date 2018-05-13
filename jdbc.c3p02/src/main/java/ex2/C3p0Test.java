27b7ccfecc6a5bf4076bfd5de5fa14b0b597aa06
shix <2469719291@qq.com>
2018-05-13 09:03:08 +0800

    这是组长上传项目

---
+++ jdbc.c3p02/src/main/java/ex2/C3p0Test.java
@@ -0,0 +1,36 @@
+package ex2;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+public class C3p0Test {
+	public static void main(String[] args) {
+		ComboPooledDataSource cds = new ComboPooledDataSource();
+
+		// TODO Auto-generated method stub
+		// 启动数据库
+		Connection conn = null;
+		// 对象
+		Statement stmt = null;
+		ResultSet res = null;
+		try {
+			conn = cds.getConnection();
+			// 输入sql
+			String sql = "select * from t_user";
+			stmt = conn.createStatement();
+			res = stmt.executeQuery(sql);
+			while (res.next()) {
+				System.out.println(res.getInt("id") + "\t" + res.getString("name") + "\t" + res.getInt("age") + "\t"
+						+ res.getDate("birthday"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 6 关闭： 先告诉Statement说话， 再断开连接
+			cds.close();
+
+		}
+
+	}
+}
